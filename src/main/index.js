ec73ef17b6a96d963e6a69898ab71727814015b5
Leeyeeun <dpdms0109@naver.com>
2021-07-27 16:06:34 +0900

    Link 태그 적용

--- src/main/index.js
+++ src/main/index.js
@@ -1,6 +1,7 @@
 import './index.css';
 import axios from 'axios';
 import React from 'react';
+import {Link} from 'react-router-dom';
 
 function MainPages() {
     const [products, setProducts] = React.useState([]);
@@ -33,21 +34,23 @@ function MainPages() {
                         products.map(function(product, index){
                             return (
                             <div className="product-card">
-                                <div>
-                                    <img className="product-img" src={product.imageUrl}></img>
-                                </div>
-                                <div className="product-contents">
-                                    <span className="product-name">
-                                        {product.name}
-                                    </span>
-                                    <span className="product-price">
-                                        {product.price}원
-                                    </span>
-                                    <div className="product-seller">
-                                        <img className="product-avatar" src="images/icons/avatar.png"></img>
-                                        <span>{product.seller}</span>
+                                <Link className="product-link" to={`/products/${index}`}>
+                                    <div>
+                                        <img className="product-img" src={product.imageUrl}></img>
                                     </div>
-                                </div>
+                                    <div className="product-contents">
+                                        <span className="product-name">
+                                            {product.name}
+                                        </span>
+                                        <span className="product-price">
+                                            {product.price}원
+                                        </span>
+                                        <div className="product-seller">
+                                            <img className="product-avatar" src="images/icons/avatar.png"></img>
+                                            <span>{product.seller}</span>
+                                        </div>
+                                    </div>
+                                </Link>
                             </div>);
                         })
                     }
